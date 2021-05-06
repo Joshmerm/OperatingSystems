f7a46654824a2f6dd965ff92e05fe2c02ed81249
Josh <joshmerm@gmail.com>
2021-05-06 01:10:55 -0700

    Update To ClientExec

--- Project3/ClientExec.cpp
+++ Project3/ClientExec.cpp
@@ -10,41 +10,44 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    while (true)
     {
-        printf("\n Socket creation error \n");
-        return -1;
+        int sock = 0, valread;
+        struct sockaddr_in serv_addr;
+
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("\n Socket creation error \n");
+            return -1;
+        }
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+
+        // Convert IPv4 and IPv6 addresses from text to binary form
+        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+        {
+            printf("\nInvalid address/ Address not supported \n");
+            return -1;
+        }
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            printf("\nConnection Failed \n");
+            return -1;
+        }
+
+        string input;
+        string b;
+        cout << "Enter Commands: ";
+        getline(cin, input);
+        send(sock, input.c_str(), input.length(), 0);
+        usleep(2000);
+        char buffer[1000000] = {0};
+        valread = read(sock, buffer, 1000000);
+        printf("%s\n", buffer);
+        close(sock);
+
     }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    string input;
-    string b;
-    cout << "Enter Commands: ";
-    getline(cin, input);
-    send(sock, input.c_str(), input.length(), 0);
-    usleep(2000);
-    char buffer[1000000] = {0};
-    valread = read( sock , buffer, 1000000);
-    printf("%s\n",buffer );
-    close(sock);
-
     return 0;
 }
